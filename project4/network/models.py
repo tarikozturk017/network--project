4debb17826b32e4b129f0c03522c2344aff9d2b2
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-12-07 20:11:08 -0500

    post model & post added

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id} made by {self.user} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
