0eb8f4e23f19e46a908927d6539c65f823ea4c77
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-12-07 21:03:27 -0500

    post can post and display

--- project4/network/views.py
+++ project4/network/views.py
@@ -4,12 +4,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+    allPosts = Post.objects.all().order_by("id").reverse()
+    return render(request, "network/index.html", {
+        "allPosts": allPosts
+    })
 
+
+def newPost(request):
+    if request.method == "POST":
+        content = request.POST['content']
+        user = User.objects.get(pk=request.user.id)
+        post = Post(content=content, user=user)
+        post.save()
+        return HttpResponseRedirect(reverse(index))
 
 def login_view(request):
     if request.method == "POST":
